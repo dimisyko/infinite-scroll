4c7fe7ab781ff22aba3c6e5793b91abea8a1f854
dimisyko <dimitri.syko@hotmail.com>
2022-05-17 16:02:24 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -14,6 +14,10 @@
             init -= (e.deltaY - 0.6) / 70
             el.style.transform = 'translate3d(0, ' + init + '%, 0px)'
         })
+        window.addEventListener('touchmove', function(e){
+            init -= (e.deltaY - 0.6) / 70
+            el.style.transform = 'translate3d(0, ' + init + '%, 0px)'
+        })
     }
 
     anim(img[0], 0)
