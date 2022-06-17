0eda86e4902771e41d2c935860f88818f91b89b0
dimisyko <dimitri.syko@hotmail.com>
2022-06-17 18:22:00 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 (function () {
     const img = document.querySelectorAll('.blocImg')
+
     function anim(el, init) {
         setInterval(() => {
             if (init > 100) {
                 init -= 100 * 2
-            }
-           else if (init < -100) {
+            } else if (init < -100) {
                 init -= -100 * 2
             }
             el.style.transform = 'translate3d(0, ' + init + '%, 0px)'
@@ -14,6 +14,21 @@
             init -= (e.deltaY - 0.6) / 70
             el.style.transform = 'translate3d(0, ' + init + '%, 0px)'
         })
+
+        let startTouch = 0
+        function touchStart(e) {
+            startTouch = e.touches[0].clientY
+        }
+
+        function move(e) {
+            const touchY = e.touches[0].clientY
+            init += (touchY - startTouch) / 10
+            el.style.transform = 'translate3d(0, ' + init + '%, 0px)'
+            startTouch = touchY
+        }
+        document.addEventListener('touchstart', touchStart)
+        document.addEventListener('touchmove', move)
+     
     }
 
     anim(img[0], 0)
